25ecf743fc8a518dbc664c19676d3b9635e3f819
CryFeiFei <809151882@qq.com>
2020-05-07 22:22:43 +0800

    [过滤出来所有的宝可梦的名称]

--- pokemon.py
+++ pokemon.py
@@ -6,6 +6,13 @@ import urllib3
 import ssl
 import requests, re
 
+
+def is_all_chinese(strs):
+    for _char in strs:
+        if not '\u4e00' <= _char <= '\u9fa5':
+            return False
+    return True
+
 class PokemonParser:
     pokemonList = []
     def __init__(self, htmlPath):
@@ -23,10 +30,17 @@ class PokemonParser:
         for table in bs.find_all('table', class_='a-c roundy eplist bgl-神奇宝贝百科 b-神奇宝贝百科 bw-2') :
 #            print(table)
             for tag in table.find_all("td"):
-                print(tag.string)
-                print("--------")
+ #               print(tag.string)
+  #              print("--------")
                 for child in tag:
-                    print(child.string)
+#                    if (is_all_chinese(child.string)):
+                    strPokemon = child.string
+                    if (strPokemon):
+                        if (is_all_chinese(strPokemon)):
+                            print(strPokemon)
+ #                       print(child.string)
+ #                   else:
+  #                      print("123")
 
  #       print(bs.find_all("a"))
 
--- test.py
+++ test.py
@@ -11,11 +11,10 @@ import pokemon
 # 这个是
 allSimplePokemonInfoUrl = 'http://wiki.52poke.com/wiki/%E5%AE%9D%E5%8F%AF%E6%A2%A6%E5%88%97%E8%A1%A8%EF%BC%88%E6%8C%89%E5%85%A8%E5%9B%BD%E5%9B%BE%E9%89%B4%E7%BC%96%E5%8F%B7%EF%BC%89/%E7%AE%80%E5%8D%95%E7%89%88'
 
-pokeMon = pokemon.PokemonParser(allSimplePokemonInfoUrl)
-
-pokeMon.printName()
-
-pokeMon.parserAllPokemon()
+if __name__ == '__main__':
+    pokeMon = pokemon.PokemonParser(allSimplePokemonInfoUrl)
+    pokeMon.printName()
+    pokeMon.parserAllPokemon()
 
 # file = open('./1.html', 'rb') 
 # html = file.read() 
